52119fdf9f22987b72fdb0852a1d4b86670f3ed6
Long Yinan <lynweklm@gmail.com>
2016-03-29 20:15:06 +0800

    fix(ng_patch): fix service injector invalid when bootstrap use ng-app

--- src/service_injector.ts
+++ src/service_injector.ts
@@ -2,11 +2,18 @@
 import {forEach} from './utils'
 import {bootstrapModules} from './ng_patch'
 
+let ngApps: NodeListOf<Element>
+
 export const serviceInjector = (deps: string []) => {
   if (!deps) deps = []
   if (!(deps instanceof Array)) throw new Error('deps must be Array')
   return (target: any) => {
     angular.element(document).ready(() => {
+      ngApps = document.querySelectorAll('[ng-app]')
+      angular.forEach(ngApps, (ele: Element) => {
+        const appName = ele.getAttribute('ng-app')
+        if (bootstrapModules.indexOf(appName) === -1) bootstrapModules.push(appName)
+      })
       forEach(bootstrapModules, module => {
         if (!deps.length) return
         angular.module(module).run(['$injector', ($injector: any) => {
--- test/app.ts
+++ test/app.ts
@@ -6,7 +6,7 @@ import {TestService} from './app/service'
 angular.module('App.Service', [])
   .service('TestService', TestService)
 
-angular.module('App', ['ionic', 'App.Service'])
+export default angular.module('App', ['ionic', 'App.Service'])
   .controller('TestView', TestView)
 
-export default angular.bootstrap(angular.element(document.body), ['App'])
+// export default angular.bootstrap(angular.element(document.body), ['App'])
